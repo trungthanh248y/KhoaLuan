2c7c4dbda09ae64d41233c8072ad72ba3d1c6367
trungthanh123 <trungthanh248y@gmail.com>
2019-06-05 13:43:40 +0700

    file_KhoaLuan

---
+++ KhoaLuanMau/app/Item.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    //
+    protected $guarded=['id'];
+    public function posts(){
+        return $this->belongsToMany('App\Post','item_post');
+    }
+    public function rooms(){
+        return $this->belongsToMany('App\Room','item_post');
+    }
+}
